82cc36c65d5f766631938905b35e839f384db4b9
Tfronn <keeelvincamilo@hotmail.com>
2022-06-08 15:38:54 -0300

    alterações cors 4

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
-import cors from 'cors';
 import express from 'express';
 import { routes } from './routes';
 
 const app = express();
-const corsOptions = {
-  origin: "https://freestop-front.vercel.app/",
-  "Access-Control-Allow-Origin": "https://freestop-front.vercel.app/"
-}
 
-app.use(cors(corsOptions))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(routes);
